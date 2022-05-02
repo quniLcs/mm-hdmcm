7fc0137ab1fb1b6f3336c0ef5d4c01486aefc14a
Yiqun Wang <87119318+quniLcs@users.noreply.github.com>
2022-05-02 22:12:45 +0800

    Add files via upload

---
+++ multiple.m
@@ -0,0 +1,188 @@
+clear;
+clc;
+close all;
+
+rng(0);
+
+% n = 2.4e4;
+% m = 2e1;
+n = 2.4e7;
+m = 2e4;
+
+mu = 0.2; % 假阴性
+nu = 0.0015; % 假阳性
+lambda = [0.15, 0.15, 0.3];
+% lambda = [0.15, 0.15, 0.45];
+% lambda = [0.15, 0.15, 0.6];
+theta = 0.2;
+
+rounds = 90;
+records = zeros(rounds, 3);
+
+people = logical(sparse(n, 2));
+% 第一个逻辑值表示是否感染病毒
+% 第二个逻辑值表示是否被隔离
+
+% 初始状态
+order = randperm(n);
+for index = 1:n
+    if order(index) <= m
+        people(index, 1) = 1;
+    end
+end
+QI = 0;  % 感染病毒且被隔离
+QS = 0;  % 未感染病毒但被隔离
+NS = n - m;  % 未感染病毒且未被隔离
+NI = m;  % 感染病毒但未被隔离
+
+for round = 1:rounds
+    fprintf('round %d\n', round)
+    fprintf('%8s\t%8s\t%8s\t%8s\n', 'QI', 'QS', 'NS', 'NI');
+    fprintf('%8d\t%8d\t%8d\t%8d\n', QI, QS, NS, NI);
+    
+    % 新增感染
+    infect = NI * lambda(1) / NS;
+    for index = 1:n
+        if people(index, 1) == 0 && people(index, 2) == 0
+            if rand < infect
+                people(index, 1) = 1;
+                NI = NI + 1;
+                NS = NS - 1;
+            end
+        end
+    end
+
+    fprintf('%8d\t%8d\t%8d\t%8d\n', QI, QS, NS, NI);
+
+    % 混合检测
+    p = NI / (NI + NS);
+    p = p * (1 - mu) + (1 - p) * nu;
+    [~, k] = min(1 + 1 ./ (1:100) - (1 - p) .^ (1:100));
+    group_num = 0;
+    group_index = zeros(1, k);
+    group_result = zeros(1, k);
+    recheck = logical(sparse(n, 1));
+    for index = 1:n
+        if people(index, 2) == 0
+            group_num = group_num + 1;
+            group_index(group_num) = index;
+            if people(index, 1) == 0  && rand < nu  % 假阳性
+                group_result(group_num) = 1;
+            elseif people(index, 1) == 1  && rand >= mu  % 真阳性
+                group_result(group_num) = 1;
+            end
+
+            if group_num == k || index == n
+                if any(group_result)
+                    for subindex = group_index(1:group_num)
+                        recheck(subindex) = 1;
+                        people(subindex, 2) = 1;
+                        if people(subindex, 1) == 0
+                            NS = NS - 1;
+                            QS = QS + 1;
+                        else
+                            NI = NI - 1;
+                            QI = QI + 1;
+                        end
+                    end
+                end
+
+                group_num = 0;
+                group_result = zeros(1, k);
+            end
+        end
+    end
+
+    fprintf('%8d\t%8d\t%8d\t%8d\n', QI, QS, NS, NI);
+
+    % 新增感染
+    infect = NI * lambda(2) / NS;
+    for index = 1:n
+        if people(index, 1) == 0 && people(index, 2) == 0
+            if rand < infect
+                people(index, 1) = 1;
+                NI = NI + 1;
+                NS = NS - 1;
+            end
+        end
+    end
+
+    fprintf('%8d\t%8d\t%8d\t%8d\n', QI, QS, NS, NI);
+
+    % 单人单管检测
+    for index = 1:n
+        if recheck(index) == 1
+            if people(index, 1) == 0 && rand >= nu  % 真阴性
+                people(index, 2) = 0;
+                QS = QS - 1;
+                NS = NS + 1;      
+            elseif people(index, 1) == 1 && rand < mu  % 假阴性
+                people(index, 2) = 0;
+                QI = QI - 1;
+                NI = NI + 1;
+            end
+        end
+    end
+
+    fprintf('%8d\t%8d\t%8d\t%8d\n', QI, QS, NS, NI);
+    
+    % 新增感染
+    infect = NI * lambda(3) / NS;
+    for index = 1:n
+        if people(index, 1) == 0 && people(index, 2) == 0
+            if rand < infect
+                people(index, 1) = 1;
+                NI = NI + 1;
+                NS = NS - 1;
+            end
+        end
+    end
+
+    fprintf('%8d\t%8d\t%8d\t%8d\n', QI, QS, NS, NI);
+
+    for index = 1:n
+        if people(index, 2) == 1 
+            % 痊愈
+            if people(index, 1) == 1 && rand < theta
+                people(index, 1) = 0;
+                QI = QI - 1;
+                QS = QS + 1;
+            end
+
+            % 单人单管检测
+            if people(index, 1) == 0  % 阴性
+                if rand >= nu  % 真阴性
+                    people(index, 2) = 0;
+                    QS = QS - 1;
+                    NS = NS + 1;
+                end           
+            else  % 阳性
+                if rand < mu  % 假阴性
+                    people(index, 2) = 0;
+                    QI = QI - 1;
+                    NI = NI + 1;
+                end
+            end
+        end
+    end
+
+    fprintf('%8d\t%8d\t%8d\t%8d\n', QI, QS, NS, NI);
+    records(round, 1) = QI;
+    records(round, 2) = NI;
+    records(round, 3) = k;
+end
+
+figure;
+plot(records(:, 1:2));
+title('疫情情况随时间的变化');
+legend(["感染病毒且被隔离人数", "感染病毒但未被隔离人数"]);
+xlabel('轮数');
+ylabel('人数');
+
+figure;
+plot(records(:, 3));
+title('混合检测每组的人数随时间的变化');
+xlabel('轮数');
+ylabel('混合检测每组的人数');
+
+save('record.mat', 'records');
--- simple.m
+++ simple.m
@@ -2,13 +2,24 @@ clear;
 clc;
 close all;
 
-n = 1.2e7;
+% 上海
+n = 2.4e7;
 m = 2e4;
+
 k = 2:100;
-
 e = 1 + 1 ./ k - (1 - m / n) .^ k;
-
 plot(k, n * e);
+hold on;
+
+% 长春
+n = 9e6;
+m = 2e3;
+
+k = 2:100;
+e = 1 + 1 ./ k - (1 - m / n) .^ k;
+plot(k, n * e);
+
 title('核酸检测次数随混合检测每组的人数的变化');
-xlabel('混合检测每组的人数');
-ylabel('核酸检测次数');
+legend(['上海'; '长春'])
+xlabel('核酸检测次数');
+ylabel('混合检测每组的人数');
